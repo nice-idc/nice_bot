1063f5bfac91db9b50e6c906b252aa57c6326a3b
nice-idc <91746398+nice-idc@users.noreply.github.com>
2021-10-01 11:24:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import discord
+import random
+from discord.ext import commands
+from discord import *
 
+client = commands.Bot(command_prefix = "$")
+players = {}  
+
+@client.event
+async def on_message_delete(message):
+     snipe_message_author[message.channel.id] = message.author
+     snipe_message_content[message.channel.id] = message.content
+     await sleep(60)
+     del snipe_message_author[message.channel.id]
+     del snipe_message_content[message.channel.id]
+
+@client.command(name = 'snipe')
+async def snipe(ctx):
+    channel = ctx.channel
+    try: #This piece of code is run if the bot finds anything in the dictionary
+        em = discord.Embed(name = f"Last deleted message in #{channel.name}", description = snipe_message_content[channel.id])
+        em.set_footer(text = f"This message was sent by {snipe_message_author[channel.id]}")
+        await ctx.send(embed = em)
+    except: #This piece of code is run if the bot doesn't find anything in the dictionary
+        await ctx.send(f"There are no recently deleted messages in #{channel.name}")
